a340d543cede4cec6816c486a1dd0c21a154d444
Ivan <253@bk.ru>
2024-03-07 21:14:02 +0300

    2

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
+    [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private float _speed = 2f;
     private float _inputH;
     private float _inputV;
@@ -13,20 +14,24 @@ public class PlayerCharacter : MonoBehaviour
         _inputV = v;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         Move();
     }
 
     private void Move() {
 
-        Vector3 direction = new Vector3(_inputH,0,_inputV).normalized;
-        transform.position += direction * Time.deltaTime * _speed;
+        //Vector3 direction = new Vector3(_inputH,0,_inputV).normalized;
+        //transform.position += direction * Time.deltaTime * _speed;
+
+        Vector3 velocity = (transform.forward * _inputV + transform.right * _inputH).normalized * _speed;
+        _rigidbody.velocity = velocity;
 
     }
 
-    public void GetMoveInfo(out Vector3 position) {
+    public void GetMoveInfo(out Vector3 position, out Vector3 velocity) {
         position = transform.position;
+        velocity = _rigidbody.velocity;
     }
 
 }
